22cad2c4823d74f9c42b4ad0346e9fcd0b242f2a
Ephraim Valladares <effelion@gmail.com>
2024-12-12 14:56:23 +0100

    add to cart data fetch changes

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,16 +1,11 @@
-let category = document.getElementById("cat")
-let val = category.value
-category.value
-console.log(val)
 
 async function getData() {
   try {
     const response = await fetch("https://fakestoreapi.com/products")
     const data = await response.json()
     console.table(data)
-    data.map(items => (
-      console.log(items)
-    ))
+  
+    return data
   }
   catch (error) {
     console.error("Fetch error:" , error)
