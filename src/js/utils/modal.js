237096c50d16064e36a6b417913dfe2fc840e4b4
Aleksa2024 <solevic.aleksa@yahoo.com>
2024-12-16 14:14:31 +0100

    Work in progress modul

--- src/js/utils/modal.js
+++ src/js/utils/modal.js
@@ -5,25 +5,29 @@ async function productModal(productId) {
   let productObject = modalData.find(
     (product) => product.id === parseInt(productId)
   );
-  
+
   const modalWindow = document.createElement("div");
   modalWindow.classList.add("product-modal");
   modalWindow.innerHTML = `
-  <article class="product" data-id="${productObject.id}">
+  <article class="modal-article" data-id="${productObject.id}">
+  <button class="close-modal">X</button>
           <img class="product-img" src="${productObject.image}" alt="${productObject.title}" width="150" height="175" />
           <h3 class="truncate">${productObject.title}</h3>
           <p class="truncate">${productObject.description}</p>
           <button>Add to cart</button>
-
+          
           <div class="product-links">
             <p>Price: $${productObject.price}</p>
             <p>Rating: ★ ${productObject.rating.rate}</p>
           </div>
         </article>
   `;
+  modalWindow.querySelector('.close-modal').addEventListener('click', () => {
+    modalWindow.remove();
+  });
 
+  document.body.appendChild(modalWindow);
   console.log(modalWindow);
-  // });
 }
 
 export { productModal };
