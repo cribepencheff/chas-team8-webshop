ff320b5787ba99a7f2969b196ea277de1bbfc442
Ephraim Valladares <effelion@gmail.com>
2024-12-18 18:06:11 +0100

    delete and add item once function

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,22 +1,60 @@
+import { removeItem ,cartItemCountLS} from "./cartFunctions.js"
 
 let getCartItems = JSON.parse(localStorage.getItem("cartList"))
 let listContainer = document.querySelector(".ul")
+let totalPrice = document.querySelector(".cart_totalAmount")
+let cartItemCount = document.querySelector("#itemCounter")
+let itemAmount = document.querySelector(".itemCountCounter")
+let counter = 0
 
 
-    listContainer.innerHTML = getCartItems.map(items => (
+
+// itemAmount.innerHTML = counter
+const showCartItems = (data) => { 
+    cartItemCount.innerHTML = cartItemCountLS()
+    listContainer.innerHTML = data.map(items => (
         `<li class="CartItem" id=${items.id}>
-                            <img class="itemImage" src="${items.image}" width="100px" height="100px" alt="">
-                            <div class="itemTitleWrapper">
-                                <span class="itemTitle">${items.title}</span>
-                                <p class="itemDescription">${items.description}</p>
-                            </div>
-                            <div class="itemCountWrapper">
-                                <button class="itemCountBtn"> - </button>
-                                <span class="itemCountCounter"></span>
-                                <button class="itemCountBtn"> + </button>
-                            </div>
-                            <h3 class="itemPrice">$ ${items.price} </h3>
-                            <img id="removeItemBtn" src="../src/images/icons/icons8-trash-128.png"/>
-                        </li>`
+        <img class="itemImage" src="${items.image}" width="100px" height="100px" alt="">
+        <div class="itemTitleWrapper">
+        <span class="itemTitle">${items.title}</span>
+        <p class="itemDescription">${items.description}</p>
+        </div>
+        <div class="itemCountWrapper">
+        <button class="itemCountBtn" id="countMinus"> - </button>
+        <span class="itemCountCounter"></span>
+        <button class="itemCountBtn" id="countPlus"> + </button>
+        </div>
+        <h3 class="itemPrice">$ ${items.price} </h3>
+        <img id="removeItemBtn" data-id=${items.id} src="../src/images/icons/icons8-trash-128.png" role="button"/>
+        </li>`
     )).join(" ")
+}
+showCartItems(getCartItems) 
 
+const deleteBtn = listContainer.querySelectorAll("img");
+ deleteBtn.forEach((img) => { 
+    img.addEventListener("click", (event) => { 
+        const itemId = parseInt(event.target.dataset.id)
+        removeItem(itemId)
+        let newCartList = JSON.parse(localStorage.getItem("cartList"))
+        showCartItems(newCartList)
+    })
+ })
+
+
+// const addBtn = listContainer.querySelectorAll("#countPlus");
+//  addBtn.forEach((btn) => { 
+//     btn.addEventListener("click", () => { 
+
+//         console.log("clicked");
+//     })
+//  })
+
+
+
+// -------------------------------------------------------------- 
+
+//  TO FIX 
+//  - items are not dissapearing when runing the removeitem, only removes from LS
+//  - add and substract item function 
+  
--- src/js/cartFunctions.js
+++ src/js/cartFunctions.js
@@ -13,9 +13,8 @@ export let cartItemCountLS = () => {
  }
 // -------------------------------------------------------------------------------------------------- 
 // -------------------------------------------------------------------------------------------------- 
-
+// Get the clicked item by id
   export const getById = (data, id) => { 
-    console.log(id)  
     
     if (!data) {  // checks if the globaldata variable is populated or not
           console.error("data not loaded yet")
@@ -25,12 +24,30 @@ export let cartItemCountLS = () => {
       if (!items) {                                           // compares the id from the clicked item with and looks for the same one trought globaldata
           console.warn(`item with id:${id} is not found `);
           return null
-      } 
-        const getCartLS = JSON.parse(localStorage.getItem("cartList"))
-              getCartLS.push(items)
-              localStorage.setItem("cartList", JSON.stringify(getCartLS))
-        cartItemCountLS()
-        console.log("item is added to cart");
+    } 
+
+    const getCartLS = JSON.parse(localStorage.getItem("cartList"))
+    const compareItem = getCartLS.find(repeated => repeated.id === items.id)
+    if (compareItem) {
+
+      alert("Items is already added in your cart. To increase the amount of the same item, please go to your cart.")
+
+    } else { 
+
+      getCartLS.push(items)
+      localStorage.setItem("cartList", JSON.stringify(getCartLS))
+      cartItemCountLS()
+      console.log("item is added to cart");
+    }  
         return items
   }
         
+// -------------------------------------------------------------------------------------------------- 
+// -------------------------------------------------------------------------------------------------- 
+// delete from cart function 
+ export const removeItem = (id) => {
+   const getCartLS = JSON.parse(localStorage.getItem("cartList"))
+   const newCart = getCartLS.filter(removedItem => removedItem.id !== id)
+   localStorage.setItem("cartList", JSON.stringify(newCart))
+   }
+
